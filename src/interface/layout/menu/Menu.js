bb8dafdab253b7dd80feba464737012fc4fd1796
Adam Ayres <adampayres@gmail.com>
2022-07-29 16:14:01 -0400

    initial commit

---
+++ src/interface/layout/menu/Menu.js
@@ -0,0 +1,15 @@
+import './Menu.css';
+
+// SIDE MENU
+function Menu() {
+    return (
+      <div className="menu">
+        <ul>
+            <li>Teams</li>
+            <li>Season</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Menu;
